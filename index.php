3161d0af336408ace16d20bc733ef7a06538942c
lucas34-nic <lucasloteca34@gmail.com>
2025-05-29 23:55:48 -0300

    Metodos de Lista

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 require_once("config.php");
 
 //$sql = new Sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//Carrega um usuário usando loçgin e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 ?>
